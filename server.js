a5a3cd27abe01b92678f70ed65271c64cb26fcea
Gilles De Mey <gilles.de.mey@gmail.com>
2014-02-23 21:36:44 -0800

    express

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-'use strict';
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
 
-var port = process.env.PORT || 3000;
+server.listen(80);
 
-var io = require('socket.io').listen(port);
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('greeting', { 'greeting': 'hello wheatley' });
-  socket.on('event', function (data) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
     console.log(data);
   });
 });
