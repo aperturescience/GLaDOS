c7d5451cfb5f47b065fd3f1322935ed1bf4d0b63
Gilles De Mey <gilles.de.mey@gmail.com>
2014-02-23 21:42:37 -0800

    updated server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  socket.emit('greeting', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
