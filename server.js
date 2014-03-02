f9a4b08b70fdce74833157d9c24ab99a3621e36b
Michiel De Mey <de.mey.michiel@gmail.com>
2014-03-02 00:46:44 -0800

    Added the redis client to GLaDOS, made code more maintainable.

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server)
-  , port = parseInt(process.env.PORT) || 3000; // we need to use parseInt because all environment variables are strings ~ Gilles
+var app    = require('express')(),
+    server = require('http').createServer(app),
+    io     = require('socket.io').listen(server),
+    port   = parseInt(process.env.PORT) || 3000, // We need to use parseInt because all environment variables are strings ~ Gilles
+    redis  = require("redis"),
+    redisClient = redis.createClient(19109, 'pub-redis-19109.us-central1-1-1.gce.garantiadata.com');
 
-server.listen(port);
+redisClient.auth(process.env.REDISPASSWD, function success() {
+  console.log('Successfully connected');
 
-app.get('/', function (req, res) {
-  res.send({
-    greeting: 'Hello human, shall I flood your home with a deadly neurotoxin?'
-  });
+  startExpress();
+  startSocketIO();
 });
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('greeting', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+/* In case of redis errors */
+redisClient.on("error", function (err) {
+  console.log("Error " + err);
 });
+
+function startExpress() {
+  server.listen(port);
+  console.log('Express is listening on port', port);
+
+  app.get('/', function (req, res) {
+    res.send({
+      greeting: 'Hello human, shall I flood your home with a deadly neurotoxin?'
+    });
+  });
+}
+
+function startSocketIO() {
+  console.log('Socket.IO is listening for connections on port', port);
+
+  io.sockets.on('connection', function (socket) {
+    socket.emit('greeting', { hello: 'human' });
+
+    socket.on('wheatley.sysinfo', function (sysinfo) {
+      console.log(sysinfo);
+    });
+  });
+
+}
